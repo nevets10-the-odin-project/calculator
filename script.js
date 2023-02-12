08e02756591212c898a21c9dafa7e962e3f44d9d
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-12 14:50:00 -0800

    Add basic math operations

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.log("Hello there");
+const add = function (num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+	return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+	return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+	return num1 / num2;
+};
+
+const operate = function (operator, num1, num2) {
+	if (operator === "add") return add(num1, num2);
+	if (operator === "subtract") return subtract(num1, num2);
+	if (operator === "multiply") return multiply(num1, num2);
+	if (operator === "divide") return divide(num1, num2);
+	return "ERROR";
+};

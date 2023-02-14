177a6f6581e076369744f9c7a635131092705688
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-02-13 22:31:40 -0800

    Refactor entire script
    Reorder functions in file.
    
    Extract functionality for buttons out of processInput into separate
    functions by button type.
    
    Replace if statements in processInput with a switch statement because
    it looks better.

--- script.js
+++ script.js
@@ -7,11 +7,119 @@ let num1 = null;
 let num2 = null;
 let currentOperator = null;
 
+const resultDiv = document.querySelector(".result");
+const equationDiv = document.querySelector(".equation");
+
 const buttons = document.querySelector(".buttons");
 buttons.addEventListener("click", processInput);
 
-const resultDiv = document.querySelector(".result");
-const equationDiv = document.querySelector(".equation");
+function processInput(e) {
+	const button = e.target.closest("button");
+	switch (button.classList.value) {
+		case "number":
+			updateTempNumber(+button.innerText);
+			populateResultDiv(tempNum);
+			isTempChanged = true;
+			break;
+		case "operator":
+			processOperator(button.innerText);
+			populateEquationDiv(`${num1} ${currentOperator}`);
+			isTempChanged = false;
+			break;
+		case "equals":
+			equals();
+			isTempChanged = false;
+			break;
+		case "clear":
+			clear();
+			break;
+		case "clear-all":
+			clearAll();
+			break;
+		case "delete":
+			deleteDigit();
+			break;
+		default:
+			alert("You pressed something unexpected...");
+	}
+}
+
+function populateResultDiv(number) {
+	resultDiv.textContent = number;
+}
+
+function populateEquationDiv(currentEquation) {
+	equationDiv.textContent = currentEquation;
+}
+
+function updateTempNumber(newNumber) {
+	if (!isTempChanged) {
+		tempNum = newNumber;
+	} else {
+		tempNum = +`${tempNum}${newNumber}`;
+	}
+}
+
+function processOperator(newOperator) {
+	if (!currentOperator) {
+		num1 = tempNum;
+	} else if (isTempChanged) {
+		num2 = tempNum;
+		num1 = operate(currentOperator, num1, num2);
+
+		populateResultDiv(num1);
+	}
+	currentOperator = newOperator;
+}
+
+function equals() {
+	isEquals = true;
+	if (!currentOperator) {
+		populateEquationDiv(`${tempNum} =`);
+		populateResultDiv(tempNum);
+	} else {
+		num2 = tempNum;
+		populateEquationDiv(`${num1} ${currentOperator} ${num2} =`);
+		num1 = operate(currentOperator, num1, num2);
+		populateResultDiv(num1);
+	}
+}
+
+function clear() {
+	if (!isEquals) {
+		isTempChanged = false;
+		tempNum = 0;
+		populateResultDiv(tempNum);
+	} else {
+		clearAll();
+	}
+}
+
+function clearAll() {
+	isEquals = false;
+	isTempChanged = false;
+	tempNum = 0;
+	num1 = null;
+	num2 = null;
+	currentOperator = null;
+	populateEquationDiv("");
+	populateResultDiv(tempNum);
+}
+
+function deleteDigit() {
+	if (!isTempChanged) return;
+	const tempNumString = tempNum.toString();
+	tempNum = +tempNumString.substring(0, tempNumString.length - 1);
+	populateResultDiv(tempNum);
+}
+
+function operate(operator, a, b) {
+	if (operator === "+") return add(a, b);
+	if (operator === "-") return subtract(a, b);
+	if (operator === "x") return multiply(a, b);
+	if (operator === "÷") return divide(a, b);
+	return "ERROR";
+}
 
 function add(a, b) {
 	return a + b;
@@ -27,108 +135,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
 	return a / b;
-}
-
-function operate(operator, a, b) {
-	if (operator === "+") return add(a, b);
-	if (operator === "-") return subtract(a, b);
-	if (operator === "x") return multiply(a, b);
-	if (operator === "÷") return divide(a, b);
-	return "ERROR";
-}
-
-function updateNumber(newNumber) {
-	if (isNum1) {
-		num1 = num1 === 0 ? newNumber : +`${num1}${newNumber}`;
-	} else {
-		num2 = num2 === 0 ? newNumber : +`${num2}${newNumber}`;
-	}
-}
-
-function populateResultDiv(number) {
-	resultDiv.textContent = number;
-}
-
-function populateEquationDiv(currentEquation) {
-	equationDiv.textContent = currentEquation;
-}
-
-function processInput(e) {
-	const button = e.target.closest("button");
-
-	if (button.classList.value === "number") {
-		const newNumber = +button.innerText;
-
-		if (!isTempChanged) {
-			tempNum = newNumber;
-		} else {
-			tempNum = +`${tempNum}${newNumber}`;
-		}
-
-		isTempChanged = true;
-
-		populateResultDiv(tempNum);
-	} else if (button.classList.value === "operator") {
-		const newOperator = button.innerText;
-
-		if (!currentOperator) {
-			num1 = tempNum;
-		} else if (isTempChanged) {
-			num2 = tempNum;
-			num1 = operate(currentOperator, num1, num2);
-
-			populateResultDiv(num1);
-		}
-
-		isTempChanged = false;
-		currentOperator = newOperator;
-
-		populateEquationDiv(`${num1} ${currentOperator}`);
-	} else if (button.classList.value === "equals") {
-		isEquals = true;
-		if (!currentOperator) {
-			populateEquationDiv(`${tempNum} =`);
-			populateResultDiv(tempNum);
-			isTempChanged = false;
-		} else {
-			num2 = tempNum;
-
-			populateEquationDiv(`${num1} ${currentOperator} ${num2} =`);
-
-			num1 = operate(currentOperator, num1, num2);
-
-			populateResultDiv(num1);
-
-			isTempChanged = false;
-		}
-	} else if (button.classList.value === "clear") {
-		if (!isEquals) {
-			isTempChanged = false;
-			tempNum = 0;
-		} else {
-			isEquals = false;
-			isTempChanged = false;
-			tempNum = 0;
-			num1 = null;
-			num2 = null;
-			currentOperator = null;
-			populateEquationDiv("");
-		}
-
-		populateResultDiv(tempNum);
-	} else if (button.classList.value === "clear-all") {
-		isEquals = false;
-		isTempChanged = false;
-		tempNum = 0;
-		num1 = null;
-		num2 = null;
-		currentOperator = null;
-		populateEquationDiv("");
-		populateResultDiv(tempNum);
-	} else if (button.classList.value === "delete") {
-		if (!isTempChanged) return;
-		const tempNumString = tempNum.toString();
-		tempNum = +tempNumString.substring(0, tempNumString.length - 1);
-		populateResultDiv(tempNum);
-	}
 }
